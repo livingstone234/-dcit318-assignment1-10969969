ab73c0e6836ed67a6608d46a6cf88cd867002264
livingstone234 <seblelivingstone@gmail.com>
2024-06-02 22:58:58 +0000

    Done with the Grade Detector \n And also have a challenge when adding so I needed to create an ignore file to excludes some files before committing y changes

---
+++ dcit318-assignment1-10969969/dcit318-assignment1-10969969/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+/*using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;*/
+
+namespace dcit318_assignment1_10969969
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeDetector();
+           
+        }
+        static void GradeDetector()
+        {
+            Console.WriteLine("Hello Bro! Welcome My Grade Detector");
+            Console.WriteLine();
+            Console.WriteLine("Enter your numerical  grade between 0 and 100");
+            Console.WriteLine();
+
+            int value = Convert.ToInt32(Console.ReadLine());
+
+            if (value >= 90)
+            {
+                Console.WriteLine("Grade A");
+            } 
+            else if(value >= 80 && value <= 89)
+            {
+                Console.WriteLine("Grade B");
+            }
+            else if (value >= 70 && value <= 79)
+            {
+                Console.WriteLine("Grade C");
+            }
+            else if (value > 60 && value <= 69)
+            {
+                Console.WriteLine("Grade D");
+            }
+            else
+            {
+                Console.WriteLine("Grade F");
+            }
+        }
+    }
+}
